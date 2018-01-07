63766b6a43db75add1949f714bc9b1fd864e2a70
matt <mattvalladarez@gmail.com>
2018-01-06 21:50:38 -0600

    added code

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 
 //Gameplan
-// 1. Create a Twitter Account
-// 2. Run a search for BEst thing I ever ate section
+// 1. Create a Twitter Account (DONE)
+// 2. Run a search for BEst thing I ever ate section (DONE)
 // 3. Choose a random tweet from the search result
 // 4. Confirm the tweet I selected is not the same tweet
 // 5. Post that tweet on my twitter account
@@ -17,9 +17,12 @@
 //  Consumer Key API KEY: o1eINkvMFZcoL1YyAjHgDivy9
 //  COnsumer Secret API Secret: tWwb7BeijDvzqrsdBzpcB4IpDdVrL9qHCVj5Wi6xYmkfRoOnlN
  
+
+//Connected to twitter
 var Twitter = require('twitter');
 var fs = require('fs');
 
+//API KEYS
 var client = new Twitter({
  consumer_key: 'o1eINkvMFZcoL1YyAjHgDivy9',
  consumer_secret: 'tWwb7BeijDvzqrsdBzpcB4IpDdVrL9qHCVj5Wi6xYmkfRoOnlN',
@@ -27,14 +30,73 @@ var client = new Twitter({
  access_token_secret: 'AmDKRo2CaibtsPWrGmyjUS0z9zhkbgkrrRsoPLqVxiqeC'
 });
 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
- if (!error) {
-   console.log(tweets);
-   //save twitter feed to a text file
-   fs.writeFile("tweets.txt", JSON.stringify(tweets, null, '\t'), (err) => {
-       if(err) throw err;
-       console.log('It\'s saved!');   
+// Tweet Options 
+var all_tweets = [];
+var historic_tweets = [];
+
+//Run a search for best thing I ever ate
+client.get('search/tweets', {q: "LIVEPD"}, function(error, tweets, response) {
+  // console.log(tweets);
+
+  //loop through all tweets possible
+  for (tweet in tweets.statuses) {
+    //add the tweet to our all_tweets list
+    all_tweets.push({
+      "text": tweets.statuses[tweet].text,
+      "id": tweets.statuses[tweet].id,
+      "name": tweets.statuses[tweet].user.name,
+      "screen_name": tweets.statuses[tweet].user.screen_name,
+      "location": tweets.statuses[tweet].user.location,
+      "url": tweets.statuses[tweet].user.url
     });
+
+    //log to console
+    console.log(tweets.statuses[tweet].text)
+
   }
+
+  //============Randomized Tweets ===================================
+   //Random Selection of Tweet
+   random_element = Math.floor(Math.random() * all_tweets.length) + 1;
+  
+   // Choose a random tweet 
+   selected_tweet = all_tweets[random_element];
+   
+   if (!(selected_tweet in historic_tweets)){
+   //Push the selected tweet to historic tweets
+   historic_tweets.push(selected_tweet);
+   console.log("THIS IS THE RANDOM TWEET: " + selected_tweet.text);
+
+  //============POST TWEET ===========================================
+  //Test case for tweeting out 
+  client.post('statuses/update', {status: selected_tweet.text + "Frm: " + selected_tweet.location}, function(error, tweet, response) {
+    if (!error) {
+      console.log(tweet);
+    }
+    }); 
+
+   }
+
+//==============FS WRITE ================================================
+
+//save twitter objects in JSON
+fs.writeFile("contents.json", JSON.stringify(all_tweets, null, '\t'), (err) => {
+  if(err) throw err;
+  console.log('It\'s saved!');   
 });
+
+//save twitter feed to a text file
+   fs.writeFile("tweets.json", JSON.stringify(tweets, null, '\t'), (err) => {
+    if(err) throw err;
+    console.log('It\'s saved!');   
+  });
+
+//save historic tweets to a text file
+fs.writeFile("historic_tweets.json", JSON.stringify(historic_tweets, null, '\t'), (err) => {
+  if(err) throw err;
+  console.log('It\'s saved!');   
+});
+
+
+});
+
